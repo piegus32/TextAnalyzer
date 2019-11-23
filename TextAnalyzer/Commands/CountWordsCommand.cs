4fef113b4b3867d69d74b0b75f453fc65baa2b17
piotrkardasz <40711740+piotrkardasz@users.noreply.github.com>
2019-11-23 17:35:10 +0100

    Added count word command (#17)
    * Regex fix for counting letters
    
    * fixed regex
    
    * Added count words command

---
+++ TextAnalyzer/Commands/CountWordsCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace TextAnalyzer.Commands
+{
+    public class CountWordsCommand : ICommand
+    {
+        public string Description => "Count Words";
+        public void Activate()
+        {
+            String text = Regex.Replace(TextFileFetcher.GetTextFileString(), @"(\d|_)+", "");
+            List<String> wordsList = Regex.Split(text, @"(\ )",
+                RegexOptions.IgnoreCase,
+                TimeSpan.FromMilliseconds(500)).ToList();
+            Regex rgx = new Regex(@"^(\ |:)$");
+            List<String> wordsOnlyList = wordsList.FindAll(x => !rgx.IsMatch(x) && x != "");
+
+            Writer.WriteMessege("Text words count: {0}\n", wordsOnlyList.Count);
+            
+        }
+    }
+}
